d136a87f8d8c7929195a97043d31dd605e551142
NILESH <NILESH@NILESH-PC.gateway.2wire.net>
2015-06-09 23:46:21 -0400

    create do get method

--- src/java/Servlet/ProductServlets.java
+++ src/java/Servlet/ProductServlets.java
@@ -5,8 +5,13 @@
  */
 package Servlet;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  *
@@ -15,4 +20,22 @@ import javax.servlet.http.HttpServlet;
 @WebServlet("/products")
 public class ProductServlets extends HttpServlet {
     
+     @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setHeader("Content-Type", "text/plain-text");
+        try {
+            PrintWriter output = response.getWriter();
+            String query = "SELECT * FROM product;";
+            if (!request.getParameterNames().hasMoreElements()) {
+                output.println(resultMethod(query));
+            } else {
+                int id = Integer.parseInt(request.getParameter("ProductID"));
+                output.println(resultMethod("SELECT * FROM product WHERE ProductID= ?", String.valueOf(id)));
+            }
+
+        } catch (IOException ex) {
+            System.err.println("Input output Exception: " + ex.getMessage());
+        }
+    }
 }
